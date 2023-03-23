e15fddef1d0cf1120f6a86d19f14b52324193d0a
Prince Pal <102817420+hustlerZzZ@users.noreply.github.com>
2023-03-23 21:25:44 +0530

    Update index.d9d33ac3.js

--- index.d9d33ac3.js
+++ index.d9d33ac3.js
@@ -1,2 +1,12 @@
+const headerBtn = document.querySelector(".header-form__items_button");
+const stepBtn = document.querySelector(".guidance-compartment__1-btn");
+const featureBtn = document.querySelector(".features-content__btn");
+const formBtn = document.querySelector(".form-heading__form-btn");
 
-//# sourceMappingURL=index.d9d33ac3.js.map
+const formSection = document.querySelector("#form__section");
+
+[stepBtn, featureBtn].forEach(function (ele) {
+    ele.addEventListener("click", function () {
+        formSection.scrollIntoView({ behavior: "smooth" });
+    });
+});
